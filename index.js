c8175599c5763d0946bbc09a3f31aebad2d33bae
amit9793 <as9455@srmist.edu.in>
2022-03-12 23:41:10 +0530

    commit

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express");
+
 const app = express();
 const importdata = require("./data.json");
-let port = process.env.PORT || 3000;
+const marsh = require("./Marsh.json");
+const sma = require("./sma.json");
+const t20 = require("./t20.json");
+const wbbl = require("./wbbl.json");
+const women = require("./women.json");
+const worldtest = require("./worldtest.json");
+let port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => {
   res.send("hello world");
@@ -9,6 +16,24 @@ app.get("/", (req, res) => {
 
 app.get("/data", (req, res) => {
   res.send(importdata);
+});
+app.get("/marsh", (req, res) => {
+  res.send(marsh);
+});
+app.get("/sma", (req, res) => {
+  res.send(sma);
+});
+app.get("/t20", (req, res) => {
+  res.send(t20);
+});
+app.get("/wbb1", (req, res) => {
+  res.send(wbbl);
+});
+app.get("/women", (req, res) => {
+  res.send(women);
+});
+app.get("/worldtest", (req, res) => {
+  res.send(worldtest);
 });
 
 app.listen(port, () => {
